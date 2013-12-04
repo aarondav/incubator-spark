e1de589453129964039e7614f5fb76c2aaa1a250
Aaron Davidson <aaron@databricks.com>
2013-12-03 21:00:45 -0800

    Jyaaaaa

--- core/src/main/scala/org/apache/spark/storage/BlockObjectWriter.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockObjectWriter.scala
@@ -111,7 +111,7 @@ class DiskBlockObjectWriter(
   private var _timeWriting = 0L
 
   override def open(): BlockObjectWriter = {
-    fos = new FileOutputStream(file, false )
+    fos = new FileOutputStream(file, true)
 //    ts = new TimeTrackingOutputStream(fos)
     channel = fos.getChannel()
     lastValidPosition = initialPosition
